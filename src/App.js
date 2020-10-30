d8fdfa545373c1aa83617bcd46e88436b236017d
domko95 <dominique.kolbe1995@googlemail.com>
2020-10-30 14:29:43 +0100

    🐛Fix reload of ListElement on form.submit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <main>
-      <FormElement />
+      <FormElement setCurrentToDos={setCurrentToDos} />
       <ListElement toDoItems={currentToDos} />
     </main>
   );
