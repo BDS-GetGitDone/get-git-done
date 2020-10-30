d7878dfdb8e947c1ebda3a0af8f7e182abf83109
Sina-Sche <si.scheckenbach@gmail.com>
2020-10-30 10:34:02 +0100

    ✨Add button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import "./App.css";
 function App() {
   return (
     <main>
-      <input className="input" type="text" />
+      <form>
+        <input className="input" type="text" />
+        <button className="btn_primary">+</button>
+      </form>
     </main>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,7 @@ import App from "./App";
 test("display input", () => {
   render(<App />);
   const inputElement = screen.getByRole("input");
+  const buttonElement = screen.getByRole("button");
   expect(inputElement).toBeInTheDocument();
+  expect(buttonElement).toBeInTheDocument();
 });
