80dd5a7a5a7df498b3b75846805e771224425a35
BelaM80 <mueller.bela@gmx.de>
2020-10-30 16:18:16 +0100

    ✨Add checkbox button

--- src/components/List.js
+++ src/components/List.js
@@ -1,8 +1,12 @@
 import "./List.css";
+import tickbox from "../assets/tickbox.svg";
 
 export default function ListElement({ toDoItems }) {
   const listItems = toDoItems.map((todo) => (
     <li className="list_item" key={todo}>
+      <button className="tickbox__btn">
+        <img className="tickbox" src={tickbox} alt="check" />
+      </button>
       {todo}
     </li>
   ));
