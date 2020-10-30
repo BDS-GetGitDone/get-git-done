3788f18a1ae00938544c090e8cb3c367f2a83c71
BelaM80 <mueller.bela@gmx.de>
2020-10-30 16:46:03 +0100

    🚧 Add function to checkbox

--- src/components/List.js
+++ src/components/List.js
@@ -2,9 +2,15 @@ import "./List.css";
 import tickbox from "../assets/tickbox.svg";
 
 export default function ListElement({ toDoItems }) {
+  const handleClick = (e) => {
+    e.preventDefault();
+    const item = e.target;
+    const listItem = item.parentElement;
+    listItem.classList.toggle("check");
+  };
   const listItems = toDoItems.map((todo) => (
     <li className="list_item" key={todo}>
-      <button className="tickbox__btn">
+      <button className="tickbox__btn" onClick={handleClick}>
         <img className="tickbox" src={tickbox} alt="check" />
       </button>
       {todo}
