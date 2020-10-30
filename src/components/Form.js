db2152c19835c3351a27aeb2040321016ac3be40
Sina-Sche <si.scheckenbach@gmail.com>
2020-10-30 11:35:04 +0100

    ✨Add local Starage

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 export default function FormElement() {
   const handleSumbit = (e) => {
     e.preventDefault();
+    localStorage.setItem("todos", inputText);
   };
+  const [inputText, setInputText] = useState("");
 
   return (
     <form onSubmit={handleSumbit}>
-      <input className="input" type="text" />
+      <input
+        onChange={(e) => setInputText(e.target.value)}
+        className="input"
+        type="text"
+      />
       <button className="btn_primary">+</button>
     </form>
   );
